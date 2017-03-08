7d6e9e06883c80db0d5b2d999071c4b7e027fafc
Celine Michaud <celine.michaud@irit.fr>
2017-03-08 11:51:23 +0100

    Implementation of Robust Implicit MLS to fit algebraic spheres on sharp edges

--- src/Core/Mesh/DCEL/Operations/VertexOperation.hpp
+++ src/Core/Mesh/DCEL/Operations/VertexOperation.hpp
@@ -1,29 +1,33 @@
-#ifndef VERTEXSPLIT_H
-#define VERTEXSPLIT_H
+#ifndef VERTEXOPERATION_H
+#define VERTEXOPERATION_H
 
 
 #include <Core/RaCore.hpp>
-#include <Core/Mesh/DCEL/Dcel.hpp>
+
 #include <Core/Math/LinearAlgebra.hpp>
 
+#include <Core/Mesh/DCEL/Dcel.hpp>
+
 #include <Core/Mesh/ProgressiveMesh/ProgressiveMeshData.hpp>
-
-
 
 namespace Ra {
 namespace Core {
 namespace DcelOperations {
 
-    //void vertexFaceAdjacency(Dcel& dcel, Index vertexIndex, std::vector<Index>& adjOut);
+    /*! \brief Compute a vertex normal */
+    RA_CORE_API Vector3 vertexNormal(Vertex_ptr vi);
+
+
+    /*! \brief Create a vertex split from data pmdata */
+    /*
     void createVt(Dcel& dcel, ProgressiveMeshData pmdata);
     void findFlclwNeig(Dcel& dcel, ProgressiveMeshData pmdata,
                        Index &flclwId, Index &flclwOpId, Index &frcrwId, Index &frcrwOpId,
                        FaceList adjOut);
-
-
+    */
     RA_CORE_API void vertexSplit(Dcel& dcel, ProgressiveMeshData pmdata);
 }
 }
 }
 
-#endif // EDGECOLLAPSE_H
+#endif // VERTEXOPERATION_H
--- src/Core/Mesh/ProgressiveMesh/ErrorMetric.hpp
+++ src/Core/Mesh/ProgressiveMesh/ErrorMetric.hpp
@@ -123,14 +123,19 @@ namespace Ra
 
             SimpleAPSSErrorMetric();
 
-            Scalar computeError(const Primitive& q1, const Primitive& q2, Vertex_ptr vs, Vertex_ptr vt, Vector3& pResult, Primitive &q, std::ofstream& file);
+            Scalar computeError(HalfEdge_ptr he, const Primitive& q1, const Primitive& q2, Vector3& pResult, Primitive &q, std::ofstream& file);
             Scalar computeError(Primitive& q, const Vector3& vs, const Vector3& vt, Vector3& pResult);
+
+            Scalar computeEdgeMinError(HalfEdge_ptr he, const Primitive& q1, const Primitive& q2, Vector3& pResult, Primitive &q);
+            Scalar computeFaceMinError(HalfEdge_ptr he, const Primitive& q1, const Primitive& q2, Vector3& pResult, Primitive &q);
+
             Scalar computeGeometricError(const Primitive& q, const Vector3& p);
 
-            Primitive combineWithTauEtaKappa(const Primitive& q1, const Primitive& q2, Scalar alpha);
             Primitive combine(const std::vector<Primitive>& p, const std::vector<Scalar>& weights, Scalar normalizing_weight_factor);
 
+            void generateRIMLSVertexPrimitive(Primitive &q, Vertex_ptr v, int ringSize);
             void generateVertexPrimitive    (Primitive &q, Vertex_ptr v, Scalar weight, int ringSize);
+
             void generateFacePrimitive      (Primitive &q, Face_ptr f, Scalar weight, int ringSize);
         };
 
