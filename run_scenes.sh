b6b80e94b9598002df7371c9deceef4e1135aaf6
Valentin Roussellet <valentin.roussellet@irit.fr>
2017-11-27 11:08:47 +0100

    Update run scenes script with full video

--- run_scenes.sh
+++ run_scenes.sh
@@ -1,57 +1,108 @@
 #!/bin/bash
 
 
+WORKDIR="/export/home/warhol/vroussel/code/git-clean/Radium-Engine/Bundle-GNU/RelWithDebInfo/bin"
+SCRIPTDIR="`date +%Y%m%d-%H%M`"
+
 # run a scene exporting subdivided meshes and anatomy
 function run_scene_exportmesh()
 {
     local infolder=$1
     local outfolder=$2
-    
-    source $infolder/scenefile
+    local scenefile=$3
+
+    source "$infolder/$scenefile"
+
+    mkdir -p $outfolder
 
     DISPLAY=":0.0" \
     ./main-app --fps 30 --autoplay -e $outfolder -n $numframes \
-    -f $infolder/$scenefile \    
+    -f $infolder/$assetfile \
     -c $infolder/$camfile \
     --anatfile $infolder/$anatfile \
-    --kffile $infolder/$kffile  \
+    --kffile $infolder/$kffile \
     --phyfile $infolder/$phyfile \
-    --subdiv --showanat --exportmeshes --runanat 2>&1 >$outfolder/log.txt 
+    --subdiv --showanat --savemeshes --runanat >& $outfolder/log.txt
 }
 
+
+# run a scene exporting frames with transparent skin for the video
 function run_scene_video()
 {
     local infolder=$1
     local outfolder=$2
-    
-    source $infolder/scenefile
+    local scenefile=$3
+
+    source $infolder/$scenefile
+
+    mkdir -p $outfolder
 
     DISPLAY=":0.0" \
     ./main-app --fps 30 --autoplay -e $outfolder -n $numframes \
-    -f $infolder/$scenefile \    
+    -f $infolder/$assetfile \
     -c $infolder/$camfile \
     --anatfile $infolder/$anatfile \
-    --kffile $infolder/$kffile  \
+    --kffile $infolder/$kffile \
     --phyfile $infolder/$phyfile \
-    --trans --showanat --exportframes --runanat 2>&1 >$outfolder/log.txt 
+    --trans --showanat --saveframes --runanat >& $outfolder/log.txt
 }
 
+# call ffmpeg on the exported frames
 function make_video()
 {
     local folder=$1
     local name=$2
     local fps=$3
 
-    cd $folder
     # get the number of the first frame
-    n=`ls | grep radiumframe | sort -n | head -n1 | tr -d -c '[0-9]'`
-    ffmpeg -framerate $fps -start_number $n -i "radiumframe_%06d.png" -c:v libx264  $name.mp4
+    local n=`ls $folder | grep radiumframe | sort -n | head -n1 | tr -d -c '[0-9]'`
+    ffmpeg -framerate $fps -start_number $n -i "$folder/radiumframe_%06d.png" -c:v libx264  $folder/$name.mp4 >& log_ffmpeg.txt
 
 }
 
-echo "Starting script"
+function run_scene()
+{
+    local num=$1
+    local in=$2
+    local scene=$3
 
-run_scene_exportmesh "test1" "out1"
-run_scene_exportmesh "test2" "out2"
+    local out="$SCRIPTDIR/scene$num"
+
+    run_scene_exportmesh "$in" "$out/meshes" "$scene"
+    run_scene_video "$in" "$out/frames" "$scene"
+    make_video "$out/frames" "scene$num" "30"
+
+    cp $in/desc.txt $out/meshes/scene.txt
+}
+
+
+echo "Starting script"
+cd $WORKDIR
+
+mkdir -p $SCRIPTDIR
+
+
+echo "Running scene 0 (parameters presentation)"
+run_scene "00" "/home/vroussel/Downloads/GEOM/arm_nomove" "scene00"
+run_scene "01" "/home/vroussel/Downloads/GEOM/arm_nomove" "scene01"
+
+echo "Running scene 1 (arm shake)"
+run_scene 1 "/home/vroussel/Downloads/GEOM/arm_rise_shake" "scenefile"
+
+
+echo "Running scene 2 (biceps curl)"
+run_scene 2 "/home/vroussel/Downloads/GEOM/arm_biceps_curl" "scenefile"
+
+rm -f list.txt
+echo "Making full video"
+find $SCRIPTDIR | grep mp4 | sort | while read file
+do
+    echo "file ' $file'" >> list.txt
+done
+ffmpeg -f concat -safe 0 -i list.txt -c copy fullvideo.mp4
+mv fullvideo.mp4 $SCRIPTDIR
+
+echo "Copying"
+cp -a $SCRIPTDIR /mnt/STORM/wip/muscle-skinning/videos/source_files/
 
 
